6480e4703a680282ec39e56ceb96bc5d3373dc8b
Owen Moogk <owenmoogk@gmail.com>
2025-03-05 14:24:10 -0500

    Added debouncing!!

--- src/NUCLEO-F401RE/stm32f4xx_it.c
+++ src/NUCLEO-F401RE/stm32f4xx_it.c
@@ -1,37 +1,3 @@
-/**
-  ******************************************************************************
-  * @file    stm32f4xx_it.c
-  * @date    13/05/2015 09:14:38
-  * @brief   Interrupt Service Routines.
-  ******************************************************************************
-  *
-  * COPYRIGHT(c) 2015 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_it.h"
 #include "xnucleoihm02a1_interface.h"
 #include "example_usart.h"
@@ -43,53 +9,68 @@ void SysTick_Handler(void)
   HAL_SYSTICK_IRQHandler();
 }
 
-// y bottom
-void EXTI0_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
-  // L6470_Run(L6470_ID(1), 1, 10000);
-  L6470_HardStop(L6470_ID(0));
-  L6470_HardStop(L6470_ID(1));
+const int clockSpeed = 84 * 10^6;
+const int debounceDelay = 150; // ms...?
 
+void wait(float ms){
+  int volatile i = 0;
+  while (i < ms * clockSpeed / 2){i++;}
 }
 
+// y min
+void EXTI0_IRQHandler(void)
+{
+  wait(debounceDelay);
+  if (!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_0))
+  {
+    // L6470_Run(L6470_ID(1), 1, 10000);
+    L6470_HardStop(L6470_ID(0));
+    L6470_HardStop(L6470_ID(1));
+  }
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+}
+
+// y max
 void EXTI1_IRQHandler(void)
 {
+  wait(debounceDelay);
+  if (!HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_1))
+  {
+    // L6470_Run(L6470_ID(0), 0, 10000);
+    L6470_HardStop(L6470_ID(0));
+    L6470_HardStop(L6470_ID(1));
+  }
   HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_1);
-  // L6470_Run(L6470_ID(0), 0, 10000);
-  L6470_HardStop(L6470_ID(0));
-  L6470_HardStop(L6470_ID(1));
+}
 
+// x min
+void EXTI4_IRQHandler(void)
+{
+  wait(debounceDelay);
+  if (!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_4))
+  {
+    // L6470_Run(L6470_ID(0), 1, 10000);
+    L6470_HardStop(L6470_ID(0));
+    L6470_HardStop(L6470_ID(1));
+  }
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
+}
+
+// y max
+void EXTI9_5_IRQHandler(void)
+{
+  wait(debounceDelay);
+  if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_7))
+  {
+    // L6470_Run(L6470_ID(1), 0, 10000);
+    L6470_HardStop(L6470_ID(0));
+    L6470_HardStop(L6470_ID(1));
+  }
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
 }
 
 void USART2_IRQHandler(void)
 {
   HAL_UART_IRQHandler(&huart2);
   USART_ITCharManager(&huart2);
-}
-
-void EXTI3_IRQHandler(void){
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
-  L6470_HardStop(L6470_ID(0));
-  L6470_HardStop(L6470_ID(1));
-}
-
-// x min
-void EXTI4_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
-  // L6470_Run(L6470_ID(0), 1, 10000);
-  L6470_HardStop(L6470_ID(0));
-  L6470_HardStop(L6470_ID(1));
-
-}
-
-// y max
-void EXTI9_5_IRQHandler(void)
-{
-  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
-  // L6470_Run(L6470_ID(1), 0, 10000);
-  L6470_HardStop(L6470_ID(0));
-  L6470_HardStop(L6470_ID(1));
-
 }
